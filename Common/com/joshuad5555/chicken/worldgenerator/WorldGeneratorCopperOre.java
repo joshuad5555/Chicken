bb75d67c2accb9ecc1ccf994719b50e9e1ddc3fe
joshuad5555 <joshuad5555@gmail.com>
2013-10-04 22:57:30 -0700

    Create WorldGeneratorCopperOre.java

---
+++ Common/com/joshuad5555/chicken/worldgenerator/WorldGeneratorCopperOre.java
@@ -0,0 +1,45 @@
+package com.joshuad5555.chicken.worldgenerator;
+
+import java.util.Random;
+
+import net.minecraft.world.World;
+import net.minecraft.world.chunk.IChunkProvider;
+import net.minecraft.world.gen.feature.WorldGenMinable;
+
+import com.joshuad5555.chicken.Chicken;
+
+import cpw.mods.fml.common.IWorldGenerator;
+
+public class WorldGeneratorCopperOre implements IWorldGenerator{
+
+	@Override
+	public void generate(Random random, int chunkX, int chunkZ, World world, IChunkProvider chunkGenerator, IChunkProvider chunkProvider) {
+		switch(world.provider.dimensionId) {
+		case 1:
+			generateNether(world, random, chunkX * 16, chunkZ * 16);
+		case 0:
+			generateSurface(world, random, chunkX * 16, chunkZ * 16);
+		case -1:
+			generateEnd(world, random, chunkX * 16, chunkZ * 16);
+		}
+	}
+
+	private void generateNether(World world, Random random, int chunkX, int chunkZ) {
+		
+	}
+	
+	private void generateSurface(World world, Random random, int chunkX, int chunkZ) {
+		for(int i = 0; i < 30; i++) {
+			int xCoord = chunkX + random.nextInt(16);
+			int yCoord = random.nextInt(64);
+			int zCoord = chunkZ + random.nextInt(16);
+			
+			(new WorldGenMinable(Chicken.oreCopper.blockID, 8)).generate(world, random, xCoord, yCoord, zCoord);
+		}
+	}
+	
+	private void generateEnd(World world, Random random, int chunkX, int chunkZ) {
+
+	}
+
+}
