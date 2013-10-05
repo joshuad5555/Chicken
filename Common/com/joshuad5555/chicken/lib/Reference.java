582e58948fc9ac6b135cdd42c918f445efa946bb
joshuad5555 <joshuad5555@gmail.com>
2013-10-04 22:56:38 -0700

    Create Reference.java

---
+++ Common/com/joshuad5555/chicken/lib/Reference.java
@@ -0,0 +1,10 @@
+package com.joshuad5555.chicken.lib;
+
+import com.joshuad5555.chicken.Chicken;
+
+public class Reference {
+
+	public static final String COMMON_PROXY_PATH = "com.joshuad5555.chicken.proxy.CommonProxy";
+	public static final String CLIENT_PROXY_PATH = "com.joshuad5555.chicken.proxy.ClientProxy";
+	
+}
